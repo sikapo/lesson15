69fe0b9c835ae77c59b0ae2076b641654e73b96e
Owner <vitycity79@gmail.com>
2023-07-14 00:03:37 +0300

    ver.3

--- src/ArrayValueCalculator.java
+++ src/ArrayValueCalculator.java
@@ -3,7 +3,7 @@ public class ArrayValueCalculator {
     public int doCalc (String[][] numbers) throws ArraySizeException, ArrayDataException {
         int i=0;
         if (numbers.length!=4 || numbers[i].length!=4){
-            throw new ArraySizeException();
+            throw new ArraySizeException("Invalid array size. Please enter the 4x4 size array");
         }
 
         int sum = 0;
@@ -13,7 +13,7 @@ public class ArrayValueCalculator {
                     sum += Integer.parseInt(number[i]);
                 }
             }catch (NumberFormatException e){
-                throw new ArrayDataException();
+                throw new ArrayDataException("Invalid array's data. Please enter the digits only!");
             }
 
         }
