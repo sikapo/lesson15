20e4bcb52b319ad4c77069979b772451d885f014
Owner <vitycity79@gmail.com>
2023-07-13 23:53:27 +0300

    ver.3

--- src/ArrayDataException.java
+++ src/ArrayDataException.java
@@ -1,6 +1,6 @@
 
 public class ArrayDataException extends Throwable {
-    public ArrayDataException() {
-        super("Invalid array's data. Please enter the digits only!");
+    public ArrayDataException(String message) {
+        super(message);
     }
 }
--- src/ArraySizeException.java
+++ src/ArraySizeException.java
@@ -1,5 +1,5 @@
 public class ArraySizeException extends Throwable {
-    public ArraySizeException() {
-        super ("Invalid array size. Please enter the 4x4 size array");
+    public ArraySizeException(String message) {
+        super (message);
     }
 }
